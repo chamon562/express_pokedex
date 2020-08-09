2a9883f59531eef7c555c5461c5f76d06d670abf
Chamon562 <Chamon562@gmail.com>
2020-08-09 15:27:01 -0700

    [define] Add another axios call to get more data and display for single pokemon.

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -6,28 +6,38 @@ const pokemon = require('../models/pokemon');
 const axios = require('axios'); 
 
 
+
 //add pokemon to our database and find more things about them
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', (req, res) =>{
       db.pokemon.findAll()
       .then(pokemons => {
         //once cick on pokem as favorite and render to poke fav page and send to favorties page
-        res.render('../views/favorites', {pokemons} )
+        res.render('favorites', {pokemons} )
         // console.log('Created: ', poke.name)
        })
        
   // TODO: Get all records from the DB and render to view
 });
-
+router.get('/:name', (req, res) =>{
+  let pokeAll = 'http://pokeapi.co/api/v2/pokemon/bulbasaur';
+  axios.get(pokeAll)
+  .then(function(apiResponse){
+  
+    let pokemon = apiResponse.data
+    console.log(apiResponse.data)
+    res.render('show', { pokemon: pokemon})
+  })
+})
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/',  (req, res) => {
   db.pokemon.findOrCreate({
     where: {
       name: req.body.name
-     
-      }
-  });
-  res.redirect('/pokemon');
+     }
+  }).then(()=>{
+    res.redirect('/pokemon');
+  })
 });
 
 
